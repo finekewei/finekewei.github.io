b7d6a95a6d8694328562dcefceb53f2fe0042fed
finekewei <526338786@qq.com>
2017-09-16 04:25:35 +0800

    Add files via upload

--- forfun/main.js
+++ forfun/main.js
@@ -7,54 +7,26 @@ function fangsuo (a,b) {
 		b.setAttribute("class", "aaa"); 
 	}
 }
-
-
-// function browserRedirect(o) {
-
-//		var o='';
-//		var sUserAgent = navigator.userAgent.toLowerCase();
-//     var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
-//     var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
-//     var bIsMidp = sUserAgent.match(/midp/i) == "midp";
-//     var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-//     var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
-//     var bIsAndroid = sUserAgent.match(/android/i) == "android";
-//     var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
-//     var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
-//     if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
-//         return o='web';
-//     }
-//     else{
-//     	return o='mobile';
-
-//     }
-// }
-
-
 function browserRedirect(o) {
-  var system ={  
-        win : false,  
-        mac : false,  
-        xll : false  
-    };  
-    var o='';
-    var p = navigator.platform;  
 
-       
-       
-    system.win = p.indexOf("Win") == 1;  
-    system.mac = p.indexOf("Mac") == 1;  
-    system.x11 = (p == "X11") || (p.indexOf("Linux")) == 1;  
- 
-    if(system.win||system.mac||system.xll){
-    	return o ='web';   
+		var o='';
+		var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+        return o='web';
     }
-    else{	
-     	return o ='web';
+    else{
+    	return o='mobile';
+
     }
 }
-
-
 
 window.onload=function() {
 	var oHtml=document.getElementById('html');
@@ -71,7 +43,9 @@ window.onload=function() {
 
 	var forfun=Math.floor(Math.random()*25);
 
-	
+	fangsuo(oBody,oHtml);
+	oBeijing.style.backgroundImage="url('forfun/images/beijing/" + forfun + ".jpg')";
+
 	if(browserRedirect(web_or_mobile) =='web'){
 
 		$("#logo").click(function(){
@@ -109,32 +83,14 @@ window.onload=function() {
 			}
 		};
 
+    
+
+	    oHuanyizhang.onclick=function(){
+	    	var forfun=Math.floor(Math.random()*25);
+	    	oBeijing.style.backgroundImage="url('forfun/images/beijing/" + forfun + ".jpg')";
+	    }
 
 
-
-		//双击背景显示或隐藏所有的板块
-		oBeijing.ondblclick=function(){
-
-			if (aXuni[0].style.display!=="none"||aXuni[1].style.display==!"none"||aXuni[2].style.display==!"none"||aXuni[3].style.display!=="none"||aXuni[4].style.display!=="none") {
-				for (var i = 0; i < aXuni.length; i++) {
-					
-					aXuni[i].style.display="none";
-					aZhenshi[i].style.display="block";
-				}
-			}
-
-			else{
-				for (var i = 0; i < aXuni.length; i++) {
-	       
-					aXuni[i].style.display="block";
-					aZhenshi[i].style.display="none";
-				}
-			}
-		}
-		oBody.onresize=function(){
-			fangsuo(oBody,oHtml);
-		}
-		fangsuo(oBody,oHtml);
 
 
 		oInput.onblur=function(){
@@ -146,31 +102,56 @@ window.onload=function() {
 			},500);
 		}
 		oInput.onfocus=function(){
+
+
 			oSearch.style="display:inline-block";
 			if (oInput.value=="Search for sth..") {
 				oInput.value="";
 			}
 		}
 
+		//双击背景显示或隐藏所有的板块
 
+
+		oBeijing.ondblclick=function(){
+
+			if (aXuni[0].style.display!=="none"||aXuni[1].style.display==!"none"||aXuni[2].style.display==!"none"||aXuni[3].style.display!=="none"||aXuni[4].style.display!=="none") {
+				for (var i = 0; i < aXuni.length; i++) {
+					
+					aXuni[i].style.display="none";
+					aZhenshi[i].style.display="block";
+
+				}
+			}
+
+			else{
+				for (var i = 0; i < aXuni.length; i++) {
+	       
+					aXuni[i].style.display="block";
+					aZhenshi[i].style.display="none";
+				}
+			}
+		}
+
+	
+
+		oBody.onresize=function(){
+			fangsuo(oBody,oHtml);
+		}
+		document.getElementById('tishi').innerHTML='<p>随便点点看？ （ <span>喜欢</span> 就设为浏览器首页吧）</p>';
 		setTimeout(function(){
 			document.getElementById('tishi').style.cssText="display:none;";
 		},5000);
-		
+	}
+	else{
+		oHtml.setAttribute("class", "mmm"); 
+		document.getElementById('tishi').innerHTML='<p><span>我在意生活的仪式感，每天暗示自己：fine today。</span></p><p><span>移动端正在开发中，pc更精彩！</span></p><p>www.fine.today</p>'
+		setTimeout(function(){
+			document.getElementById('tishi').style.cssText="display:none;";
+		},10000);
+
 	}
 
-	else{
-		alert(navigator.platform);
-
-	};
-
-
 	
-    oBeijing.style.backgroundImage="url('forfun/images/beijing/" + forfun + ".jpg')";
 
-    oHuanyizhang.onclick=function(){
-    	var forfun=Math.floor(Math.random()*25);
-    	oBeijing.style.backgroundImage="url('forfun/images/beijing/" + forfun + ".jpg')";
-    }
-	
 }
